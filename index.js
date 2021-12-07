6d88322cad5d86fe790f183c9b86b6401e95251c
John Muriu <johnmuriw@gmail.com>
2021-12-07 14:50:21 +0300

    update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // console.log("hello world")
 new Vue({
     el: "#root",
-    data:{
+    data: {
         title: "Foodigo Restaurant Dashboard",
-        orders:[
-            {name:"Chris Nwamba", description:"Rice and Ofe-Akwu", address:"Lekki", telephone:"08082092000", open:true},
-            {name:"William Imoh", description:"Rice and Chicken", address:"Amuwo", telephone:"08082818700", open:true}
+        orders: [
+            { name: "John Muriu", description: "Rice and ugali", address: "langatta", telephone: "254792616470", open: true },
         ]
     },
-    created(){
-        var pusher = new Pusher('2dd6b95d32efbc7fa4ad',{
-            cluster:'eu',
-            encrypted:true
+    created() {
+        var pusher = new Pusher('2dd6b95d32efbc7fa4ad', {
+            cluster: 'eu',
+            encrypted: true
         })
         var channel = pusher.subscribe('orders')
         channel.bind('customerOrder', (data) => {
@@ -19,12 +18,12 @@ new Vue({
             this.orders.push(data)
         })
     },
-    methods:{
+    methods: {
         // close completed order
-        close(orderToClose){
-            if ( confirm('Are you sure you want to close the order?') === true){
+        close(orderToClose) {
+            if (confirm('Are you sure you want to close the order?') === true) {
                 this.orders = this.orders.map(order => {
-                    if(order.name !== orderToClose.name && order.description !== orderToClose.description){
+                    if (order.name !== orderToClose.name && order.description !== orderToClose.description) {
                         return order;
                     }
                     const change = {
@@ -32,7 +31,7 @@ new Vue({
                     }
                     return change;
                 })
-            } 
+            }
         }
     }
 })
