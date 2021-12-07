9edfd34b9bf1adb1cd8432e7a22e24e4069db873
John Muriu <johnmuriw@gmail.com>
2021-12-07 14:47:13 +0300

    update

--- cred_sample.js
+++ cred_sample.js
@@ -1,14 +1,14 @@
 module.exports = {
-    AT:{
-        apiKey: "xxxxxxxxxxxxx",
-        username: "xxxxxx",
+    AT: {
+        apiKey: '79a17bd36053e0cd358188db1f6f080c1d458e77625fc18fddb60bd12521a130',
+        username: 'swiftt-ussd',
         format: 'json'
     },
-    pusher:{
-       appId: 'xxxxxx',
-       key: "xxxxxxxxxxx",
-       secret: "xxxxxxxxxxx",
-       cluster: "eu",
-       encrypted: true
+    pusher: {
+        appId: '1312964',
+        key: 'cbdac67abe015fd18da2',
+        secret: "158793e553d9d7368eb0",
+        cluster: "api",
+        encrypted: true
     }
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var credentials = require('./cred')
 var africastalking = require('africastalking')(credentials.AT)
 var cors = require('cors')
 var bodyParser = require('body-parser')
-// var Webtask = require('webtask-tools')
+    // var Webtask = require('webtask-tools')
 
 //configure modules
 var express = require('express')
@@ -14,11 +14,11 @@ var path = require('path')
 
 var pusher = new Pusher(credentials.pusher)
 app.use(cors())
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Serve home page and static files
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "/index.html"))
 })
 app.use(express.static(__dirname + '/'))
@@ -39,57 +39,56 @@ var orderDetails = {
 }
 var lastData = "";
 
-app.post('/order', function(req, res){
-    console.log(req.body);
-    var message = 'Hello' 
+app.post('/order', function(req, res) {
+        console.log(req.body);
+        var message = 'Hello'
 
-    var sessionId = req.body.sessionId
-    var serviceCode = req.body.serviceCode
-    var phoneNumber = req.body.phoneNumber
-    var text = req.body.text
-    var textValue = text.split('*').length
+        var sessionId = req.body.sessionId
+        var serviceCode = req.body.serviceCode
+        var phoneNumber = req.body.phoneNumber
+        var text = req.body.text
+        var textValue = text.split('*').length
 
-    if(text === ''){
-        message = welcomeMsg
-    }else if(textValue === 1){
-        message = "CON What do you want to eat?"
-        orderDetails.name = text;
-    }else if(textValue === 2){
-        message = "CON Where do we deliver it?"
-        orderDetails.description = text.split('*')[1];
-    }else if(textValue === 3){
-        message = "CON What's your telephone number?"
-        orderDetails.address = text.split('*')[2];
-    }else if(textValue === 4){
-        message = `CON Would you like to place this order?
+        if (text === '') {
+            message = welcomeMsg
+        } else if (textValue === 1) {
+            message = "CON What do you want to eat?"
+            orderDetails.name = text;
+        } else if (textValue === 2) {
+            message = "CON Where do we deliver it?"
+            orderDetails.description = text.split('*')[1];
+        } else if (textValue === 3) {
+            message = "CON What's your telephone number?"
+            orderDetails.address = text.split('*')[2];
+        } else if (textValue === 4) {
+            message = `CON Would you like to place this order?
         1. Yes
         2. No`
-        lastData = text.split('*')[3];
-    }else{
-        message = `END Thanks for your order
+            lastData = text.split('*')[3];
+        } else {
+            message = `END Thanks for your order
         Enjoy your meal in advance`
-        orderDetails.telephone = lastData   
-    }
-    
-    res.contentType('text/plain');
-    res.send(message, 200);
+            orderDetails.telephone = lastData
+        }
 
-    console.log(orderDetails)
-    if(orderDetails.name != "" && orderDetails.address != '' && orderDetails.description != '' && orderDetails.telephone != ''){
-        pusher.trigger('orders', 'customerOrder', orderDetails)
-    }
-    if(orderDetails.telephone != ''){
-        //reset data
-    orderDetails.name= ""
-    orderDetails.description= ""
-    orderDetails.address= ""
-    orderDetails.telephone= ""
-    }
+        res.contentType('text/plain');
+        res.send(message, 200);
 
-})
-//listen on port 
-app.listen(port, function(err, res){
-    if(err) throw err
+        console.log(orderDetails)
+        if (orderDetails.name != "" && orderDetails.address != '' && orderDetails.description != '' && orderDetails.telephone != '') {
+            pusher.trigger('orders', 'customerOrder', orderDetails)
+        }
+        if (orderDetails.telephone != '') {
+            //reset data
+            orderDetails.name = ""
+            orderDetails.description = ""
+            orderDetails.address = ""
+            orderDetails.telephone = ""
+        }
+
+    })
+    //listen on port 
+app.listen(port, function(err, res) {
+    if (err) throw err
     console.log("Booming on the legendary port " + port)
 })
-
